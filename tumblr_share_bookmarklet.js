a50b88c52b37e0c0daa7c98cd7e9d0f8c1b74458
Kevin Sweeney <ksweeney@vimeo.com>
2012-11-19 10:42:07 -0500

    Add formatted bookmarklet code
    * Rename single-character variables to more semantic equivalents
    * Create placeholder for method to center pop-up window

---
+++ tumblr_share_bookmarklet.js
@@ -0,0 +1,39 @@
+javascript:
+(function tumblrShareBookmarklet(doc, win, undefined) {
+    var getWindowSelection = win.getSelection,
+        getDocumentSelection = doc.getSelection,
+        document_selection = doc.selection,
+        selection = (getWindowSelection ? getWindowSelection() : (getDocumentSelection) ? getDocumentSelection() : (document_selection ? document_selection.createRange().text : 0)),
+        share_url = 'http://www.tumblr.com/share',
+        location = doc.location,
+        encode = encodeURIComponent,
+        share_params = '?v=3&u=' + encode(location.href) + '&t=' + encode(doc.title) + '&s=' + encode(selection),
+        url = share_url + share_params,
+        share_width = 450,
+        share_height = 430;
+
+    try {
+        if (!/^(.*\.)?tumblr[^.]*$/.test(location.host)) throw (0);
+        /* Internal Tumblr that verifies successful bookmarklet installation */
+        tstbklt();
+    }
+    catch (err) {
+        function centerShareWindow() {
+
+        }
+
+        function openShareWindow() {
+            if (!win.open(url, 't', 'toolbar=0,resizable=0,status=1,width=' + share_width + ',height=' + share_height)) {
+                location.href = url;
+            }
+        }
+
+        if (/Firefox/.test(navigator.userAgent)) {
+            setTimeout(openShareWindow, 0);
+        }
+        else {
+            openShareWindow();
+        }
+    }
+})(document, window);
+void(0)​;
